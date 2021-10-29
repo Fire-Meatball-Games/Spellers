03d71e51dbe2c3d2fa7e8895afc20efed390d7c7
AlejandroOrejudo2000 <alejandroorejudo2000@gmail.com>
2021-10-29 12:44:31 +0200

    pantalla de portada del juego

---
+++ project/Spellers/Assets/Scripts/UI/InitView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UIManagement
+{
+    public class InitView : View
+    {
+        [SerializeField] private Button btn_empezar;
+
+        public override void Init()
+        {
+            btn_empezar.onClick.AddListener(() => ViewManager.Show<MainMenuView>());
+        }
+    } 
+}
