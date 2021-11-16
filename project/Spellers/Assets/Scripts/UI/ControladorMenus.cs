ee55579eb1ea4ffbe5a10fd9b17a14878b6c7887
MarcosMD15 <55489396+MarcosMD15@users.noreply.github.com>
2021-11-16 21:02:30 +0100

    Arreglo bug

--- project/Spellers/Assets/Scripts/UI/ControladorMenus.cs
+++ project/Spellers/Assets/Scripts/UI/ControladorMenus.cs
@@ -7,44 +7,14 @@ namespace UIManagement
 {
     public class ControladorMenus : MonoBehaviour
     {
-        ////////////////////
-        //// Variables ////
-        //////////////////
-
-        public GameObject menuPrincipal;
-        public GameObject menuJugar;
-        public GameObject menuColeccion;
-        public GameObject menuOpciones;
-        public GameObject menuCreditos;
-
-        public GameObject fuentePequeñaObj;
-        public GameObject fuenteMedianaObj;
-        public GameObject fuenteGrandeObj;
-        Toggle fuentePequeña;
-        Toggle fuenteMediana;
-        Toggle fuenteGrande;
-
         public GameObject pantallaCompleta;
 
-        //////////////////
-        //// Metodos ////
-        ////////////////
-
-        // Start is called before the first frame update
-        void Start()
+        public void Awake()
         {
-            //menuPrincipal.SetActive(true);
-            //menuJugar.SetActive(false);
-            //menuColeccion.SetActive(false);
-            //menuOpciones.SetActive(false);
-            //menuCreditos.SetActive(false);
-            //fuentePequeña = fuentePequeñaObj.GetComponent<Toggle>();
-            //fuenteMediana = fuenteMedianaObj.GetComponent<Toggle>();
-            //fuenteGrande = fuenteGrandeObj.GetComponent<Toggle>();
+            //Application.targetFrameRate = 60;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void Update()
         {
             if (Screen.fullScreen)
             {
@@ -55,52 +25,6 @@ namespace UIManagement
                 pantallaCompleta.SetActive(false);
             }
         }
-
-    //    //// Metodo para todos los Menus ////
-    //    public void clickPantallaCompleta()
-    //    {
-    //        Screen.fullScreen = false;
-    //    }
-
-    //    //// Metodos para el Menu Principal ////
-    //    public void clickJugar()
-    //    {
-    //        menuJugar.SetActive(true);
-    //        menuPrincipal.SetActive(false);
-    //    }
-
-    //    public void clickColeccion()
-    //    {
-    //        menuColeccion.SetActive(true);
-    //        menuPrincipal.SetActive(false);
-    //    }
-
-    //    public void clickOpciones()
-    //    {
-    //        menuOpciones.SetActive(true);
-    //        menuPrincipal.SetActive(false);
-    //    }
-
-    //    public void clickCreditos()
-    //    {
-    //        menuCreditos.SetActive(true);
-    //        menuPrincipal.SetActive(false);
-    //    }
-
-    //    public void clickSalir()
-    //    {
-    //        Debug.Log("Salir");
-    //    }
-
-    //    //// Metodo para los Menus que no sen el Principal ////
-    //    public void clickAtras()
-    //    {
-    //        menuPrincipal.SetActive(true);
-    //        menuJugar.SetActive(false);
-    //        menuColeccion.SetActive(false);
-    //        menuOpciones.SetActive(false);
-    //        menuCreditos.SetActive(false);
-    //    }
     }
 
 }
