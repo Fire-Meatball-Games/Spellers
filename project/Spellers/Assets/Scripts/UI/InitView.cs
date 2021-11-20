2bb0b66111335a8a09f5de772450e1065ccf2903
MarcosMD15 <55489396+MarcosMD15@users.noreply.github.com>
2021-11-20 13:31:46 +0100

    Incluidos nuevos menús

--- project/Spellers/Assets/Scripts/UI/InitView.cs
+++ project/Spellers/Assets/Scripts/UI/InitView.cs
@@ -7,11 +7,6 @@ namespace UIManagement
     {
         [SerializeField] private Button btn_empezar;
 
-        public void Awake()
-        {
-            Application.targetFrameRate = 60;
-        }
-
         public override void Init()
         {
             btn_empezar.onClick.AddListener(() => GetComponent<Animator>().SetBool("Salir", true));
