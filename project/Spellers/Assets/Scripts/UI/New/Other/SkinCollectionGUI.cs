171fe55c1ecb35579f65198134f3914fc91d8ced
AlejandroOrejudo2000 <alejandroorejudo2000@gmail.com>
2021-12-02 00:21:38 +0100

    Partes de skins

--- project/Spellers/Assets/Scripts/UI/New/Other/SkinCollectionGUI.cs
+++ project/Spellers/Assets/Scripts/UI/New/Other/SkinCollectionGUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Skins;
+using System.Linq;
 
 namespace UIManagement
 {
@@ -11,21 +12,37 @@ namespace UIManagement
         [SerializeField] private GameObject cardList_prefab;
         [SerializeField] private GameObject cardView_prefab;
         [SerializeField] private RectTransform display;
+
+        [SerializeField] private Button face_btn;
+        [SerializeField] private Button hair_btn;
+        [SerializeField] private Button hat_btn;
+        [SerializeField] private Button eyes_btn;
+        [SerializeField] private Button nose_btn;
+        [SerializeField] private Button mouth_btn;
+        [SerializeField] private Button coat_btn;
+        [SerializeField] private Button body_btn;
         private CardSection section;
 
         public void Init()
-        {
-            List<BasicSkinPart> skins = BasicSkinPart.GetAllSkinPartsOfType(BasicSkinPart.TypePart.hat);
-            AddCardSection("Sombreros", skins);
+        {            
+            face_btn.onClick.AddListener(()=>SetCardSection(BasicSkinPart.TypePart.face));
+            hair_btn.onClick.AddListener(()=>SetCardSection(BasicSkinPart.TypePart.hair));
+            hat_btn.onClick.AddListener(()=>SetCardSection(BasicSkinPart.TypePart.hat));
+            eyes_btn.onClick.AddListener(()=>SetCardSection(BasicSkinPart.TypePart.eyes));
+            nose_btn.onClick.AddListener(()=>SetCardSection(BasicSkinPart.TypePart.nose));
+            mouth_btn.onClick.AddListener(()=>SetCardSection(BasicSkinPart.TypePart.mouth));
+            coat_btn.onClick.AddListener(()=>SetCardSection(BasicSkinPart.TypePart.coat));
+            body_btn.onClick.AddListener(()=>SetCardSection());
+            SetCardSection(BasicSkinPart.TypePart.face);
         }
 
-        private void AddCardSection(string title, List<BasicSkinPart> skins)
+        private void AddCardSection(string title, List<SkinPart> skins)
         {
             section = Instantiate(cardList_prefab, display).GetComponent<CardSection>();
             section.SetLayout(title, GetCardViewList(skins));            
         }
 
-        private List<CardView> GetCardViewList(List<BasicSkinPart> skins)
+        private List<CardView> GetCardViewList(List<SkinPart> skins)
         {
             List<CardView> cards = new List<CardView>();
             foreach (var skin in skins)
@@ -35,11 +52,38 @@ namespace UIManagement
             return cards;
         }
 
-        private SkinCardView GenerateCardView(BasicSkinPart skin)
+        private SkinCardView GenerateCardView(SkinPart skin)
         {
             SkinCardView card = Instantiate(cardView_prefab).GetComponent<SkinCardView>(); 
             card.SetUp(skin, true);
             return card;
+        }
+
+        private void SetCardSection(BasicSkinPart.TypePart part)
+        {
+            if(section != null)
+            {
+                section.Clear();
+                CardSection oldsection = section;
+                Destroy(oldsection.gameObject);
+                section = null;
+            }
+            
+            List<SkinPart> skins = BasicSkinPart.GetAllSkinPartsOfType(part).Cast<SkinPart>().ToList();
+            AddCardSection(part.ToString(), skins);
+        }
+
+        private void SetCardSection()
+        {
+            if(section != null)
+            {
+                section.Clear();
+                CardSection oldsection = section;
+                Destroy(oldsection.gameObject);
+                section = null;
+            }
+            List<SkinPart> skins = CompoundSkinPart.GetAllSkinParts().Cast<SkinPart>().ToList();
+            AddCardSection("Trajes", skins);
         }
 
 
