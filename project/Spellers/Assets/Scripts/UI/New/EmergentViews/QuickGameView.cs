ae7834cbbb493de8b9e3f8abdd4929f91338c612
AlejandroOrejudo2000 <alejandroorejudo2000@gmail.com>
2021-12-12 02:26:24 +0100

    partidas rapidas

--- project/Spellers/Assets/Scripts/UI/New/EmergentViews/QuickGameView.cs
+++ project/Spellers/Assets/Scripts/UI/New/EmergentViews/QuickGameView.cs
@@ -10,22 +10,56 @@ namespace UIManagement{
     public class QuickGameView : EmergentView
     {        
         [SerializeField] private RectTransform panel;
-        [SerializeField] private QuickGameSettings settings;
         [SerializeField] private Button play_btn;
+        [SerializeField] private Button previous_btn;
+        [SerializeField] private Button next_btn;
+        [SerializeField] private Button random_btn;
+        [SerializeField] private Image iconImage;
+
+        private int selectedIdx;
+
+        [SerializeField] private List<QuickGameSettings> settingList;
         public override void Init(){
             close_time = 0.2f;
             base.Init();
             show_effects.AddEffect(new ScreenSlideEffect(panel, Vector2.up, Vector2.zero, 1.2f, 0.2f));
             hide_effects.AddEffect(new ScreenSlideEffect(panel, Vector2.zero, Vector2.up, 1f, 0.2f));
-            play_btn.onClick.AddListener(GameTest);
+
+            play_btn.onClick.AddListener(LoadGame);
+            previous_btn.onClick.AddListener(SelectPrevious);
+            next_btn.onClick.AddListener(SelectNext);
+            random_btn.onClick.AddListener(SelectRandom);
         }
 
-        private void GameTest()
+        private void LoadGame()
         {
-            GameManager.instance.SetSettings(settings);
-             GameManager.instance.UnloadScene(SceneIndexes.MAIN_MENU);  
-            GameManager.instance.LoadSceneAsync(SceneIndexes.GAME);                     
+            GameManager.instance.SetSettings(settingList[selectedIdx]);
+            GameManager.instance.UnloadScene(SceneIndexes.MAIN_MENU);  
+            GameManager.instance.LoadSceneAsync(SceneIndexes.GAME);                  
 
+        }
+
+        private void DisplayGameSettings()
+        {
+            iconImage.sprite = settingList[selectedIdx].Icon; 
+        }
+
+        private void SelectPrevious()
+        {
+            selectedIdx = (selectedIdx + settingList.Count - 1) % settingList.Count;
+            DisplayGameSettings();
+        }
+
+        private void SelectNext()
+        {
+            selectedIdx = (selectedIdx + settingList.Count + 1) % settingList.Count;
+            DisplayGameSettings();
+        }
+
+        private void SelectRandom()
+        {
+            selectedIdx = Random.Range(0, settingList.Count);
+            DisplayGameSettings();
         }
 
 
